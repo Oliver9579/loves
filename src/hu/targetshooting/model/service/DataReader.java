9ccfd291d95c35c4843772210c0d9e02ac261203
Olivér <szabo.oliver2001@gmail.com>
2019-10-07 16:49:22 +0200

    Data Read scratch

---
+++ src/hu/targetshooting/model/service/DataReader.java
@@ -0,0 +1,9 @@
+package hu.targetshooting.model.service;
+
+import hu.targetshooting.model.domain.ShotResult;
+
+import java.util.List;
+
+public interface DataReader {
+    List<ShotResult> getData(String input);
+}
---
+++ src/hu/targetshooting/model/service/FileReader.java
@@ -0,0 +1,13 @@
+package hu.targetshooting.model.service;
+
+import hu.targetshooting.model.domain.ShotResult;
+
+import java.util.List;
+
+public class FileReader implements DataReader {
+    @Override
+    public List<ShotResult> getData(String input) {
+        return null;
+    }
+
+}
