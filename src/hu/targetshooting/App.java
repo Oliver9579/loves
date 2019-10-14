597d9868e83a39869a07e98496fd2501c912c349
Olivér <szabo.oliver2001@gmail.com>
2019-10-14 16:54:42 +0200

    Task 3,5

--- src/hu/targetshooting/App.java
+++ src/hu/targetshooting/App.java
@@ -1,16 +1,21 @@
 package hu.targetshooting;
 
-import hu.targetshooting.controller.ShotFacade;
+import hu.targetshooting.controller.ShotService;
+import hu.targetshooting.model.service.Console;
 import hu.targetshooting.model.service.DataReader;
 import hu.targetshooting.model.service.FileDataReader;
 import hu.targetshooting.model.service.ScoreCalculator;
 
+import java.util.Scanner;
+
 public class App {
-    private final ShotFacade shotFacade;
+    private final ShotService shotService;
+    private final Console console;
 
     public App() {
         DataReader data = new FileDataReader(new ScoreCalculator());
-        shotFacade = new ShotFacade(data.getData("verseny.txt"));
+        shotService = new ShotService(data.getData("verseny.txt"));
+        console=new Console(new Scanner(System.in));
     }
 
     public static void main(String[] args) {
@@ -18,6 +23,9 @@ public class App {
     }
 
     private void run() {
-        System.out.println("2. feladat: " + shotFacade.getBetterIds());
+        System.out.println("2. feladat: Az egymást követően többször találó versenyzők: " + shotService.getBetterIds());
+        System.out.println("3. feladat: A legtöbb lövést leadó versenyző rajtszáma: " + shotService.getLongestShotSequencesId());
+        int id = console.raedInt("5. feladat: Adjon meg egy rajtszámot: ");
+        System.out.println(shotService.getShotResultDetails(id));
     }
 }
